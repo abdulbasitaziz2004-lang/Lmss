d497d259d253243cbb34e984ac2a5d56b9fc6cd5
codewithbasit <abdulbasitaziz2004@gmail.com>
2026-03-15 14:26:51 +0500

    removed comments

--- src/lib/actions/user.action.js
+++ src/lib/actions/user.action.js
@@ -34,22 +34,3 @@ export async function getUserWithCourses(clerkId) {
   const user = await User.findOne({ clerkId }).populate("enrolledCourses").lean();
   return user;
 }
-
-// import User from "../models/user";
-// import { connect } from "../db";
-
-// export async function createUser(user) {
-//   try {
-//     await connect();
-
-//     if (!user.clerkId || !user.email || !user.username) {
-//       throw new Error("Missing required user fields");
-//     }
-
-//     const newUser = await User.create(user);
-//     return newUser;
-//   } catch (error) {
-//     console.error("❌ Error creating user:", error);
-//     throw error;
-//   }
-// }
